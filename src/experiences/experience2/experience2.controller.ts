788f3bf0ab9be4e828e1e90d8f76e750b1ccee54
justalk <justal.kevin@gmail.com>
2021-09-12 12:09:12 +0200

    [FEATURE] Creating a param name

--- src/experiences/experience2/experience2.controller.ts
+++ src/experiences/experience2/experience2.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { Experience2Service } from './experience2.service';
+import { CreateExperience2Dto } from './dto/experience2.dto';
 
 @Controller('experience2')
 export class Experience2Controller {
   constructor(private readonly appService: Experience2Service) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  create(@Body() createExperience2Dto: CreateExperience2Dto) {
+    const rsl = this.appService.handleBody(createExperience2Dto);
+    return rsl;
   }
 }
--- src/experiences/experience2/experience2.service.ts
+++ src/experiences/experience2/experience2.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { CreateExperience2Dto } from './dto/experience2.dto';
 
 @Injectable()
 export class Experience2Service {
-  getHello(): string {
-    return 'Hello World3!';
+  handleBody(experience2: CreateExperience2Dto): string {
+    // Testing to get a param name
+    const { name } = experience2;
+    return name;
   }
 }
