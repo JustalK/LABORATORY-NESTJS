b8a06b8ae14a8a41ebded10364f2f846e41e7ed7
justalk <justal.kevin@gmail.com>
2021-09-18 20:28:35 +0200

    [FEATURE] Removing logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: false,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
